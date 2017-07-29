00e154402c01c973cc137d3e93d9ae7d77d709d6
Developer <developer@JW-iMac.gateway>
2017-07-29 22:59:42 +1000

    node stuff

--- govhack2017-node/app.js
+++ govhack2017-node/app.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3002, function () {
-  console.log('Example app listening on port 3002!')
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
